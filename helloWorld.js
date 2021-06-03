2b36082d12388b4e6f525ded0a02867dc14f57d6
nafisatanha <84631465+nafisatanha@users.noreply.github.com>
2021-06-03 13:43:37 +0600

    Add files via upload

---
+++ helloWorld.js
@@ -0,0 +1,14 @@
+function start() {
+    console.log("Hello World!!!");
+    window.alert("Hello World");
+    var firstName;
+    var lastName;
+    
+    firstName = prompt("Please Enter Your First Name : ");
+    lastName = prompt("Please Enter Your Last Name : ");
+    
+    window.alert(" You entered " + firstName);
+    window.alert(" You entered " + lastName);
+    
+    
+}
